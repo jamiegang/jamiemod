dba6b11f20b9caeb1bb85462908d012088253915
jamiegang <64982619+jamiegang@users.noreply.github.com>
2020-05-12 13:26:53 +0100

    QoL Changes
    QoL changes

--- common/defines/00_changes.lua
+++ common/defines/00_changes.lua
@@ -1,10 +1,39 @@
 NDefines.NCountry.DISINHERIT_PRESTIGE_HIT = -25
-NDefines.NCountry.PS_ARTILLERY_BARRAGE = 25
-NDefines.NCountry.PS_NAVAL_BARRAGE = 25
+NDefines.NCountry.PS_ARTILLERY_BARRAGE = 50
+NDefines.NCountry.PS_NAVAL_BARRAGE = 30
 NDefines.NCountry.CAN_CONVERT_TERRITORY_CULTURE = 1 -- Defines if you are allowed to culture convert territory provinces
 NDefines.NCountry.CAN_CONVERT_TERRITORY_RELIGION = 1 -- Defines if you are allowed to convert religion of territory provinces
-NDefines.NEconomy.BANKRUPTCY_BUILDING_DESTRUCTION_THRESHOLD = 7
+NDefines.NEconomy.BANKRUPTCY_BUILDING_DESTRUCTION_THRESHOLD = 10
 NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = -1.5
 NDefines.NMilitary.MERCENARY_SUPPORT_LIMIT_FRACTION = 0.2
 NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
 NDefines.NReligion.HARMONY_LOSS_PER_DEV_CONVERTED = 0.5
+
+--QoL and less roach Changes as of 12/05 above is aswell, just not as recent
+
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 12
+NDefines.NCountry.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0
+NDefines.NCountryMONARCH_MIN_SKILL = 1,
+NDefines.NCountryPROSPERITY_INCREASE_SIZE= 2,
+NDefines.NCountryESTATE_PROVINCE_HAPPINESS_DECREASE = 3.0,
+NDefines.NCountryMAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.4,
+NDefines.NEconomyDEBASE_MAX_CORRUPTION = 30,
+NDefines.NEconomyBANKRUPTCY_PROVINCE_DEVASTATION_GAIN = 20,
+
+NMilitary = {
+	SLACKEN_AP_DROP = 0.05,
+	SLACKEN_MANPOWER_INCREASE = 1.5,
+	BASE_MP_TO_MANPOWER = 0.25,
+	NOMAD_PLAINS_SHOCK_BONUS = 0.10, 				
+	NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.0,
+	NAVAL_CASUALTY_MIN_MORALE_DAMAGE = 0.2,
+	CREATE_JANISSARIES_MIL_COST = 8,
+
+}
+NNationDesigner = {
+	FLAT_TAX_AMOUNT = 4,					-- Flat tax
+	FLAT_MP_AMOUNT = 3,	
+}
+-- PO_HUMILIATE_POWER_GAIN = 100, -- potential change in the future
+-- MAXIMUM_CONDOTTIERI = 20,
