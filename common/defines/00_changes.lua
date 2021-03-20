30cbda8da36891a0ac061c4ee3d1ed21025b82ec
jamiegang <64982619+jamiegang@users.noreply.github.com>
2021-03-20 13:08:11 +0000

    Bulk Upload
    Bunker Balance Bulk Upload for coffee

--- common/defines/00_changes.lua
+++ common/defines/00_changes.lua
@@ -1,40 +1,50 @@
-NDefines.NCountry.DISINHERIT_PRESTIGE_HIT = -25
-NDefines.NCountry.PS_ARTILLERY_BARRAGE = 50
-NDefines.NCountry.PS_NAVAL_BARRAGE = 30
-NDefines.NCountry.CAN_CONVERT_TERRITORY_CULTURE = 1 -- Defines if you are allowed to culture convert territory provinces
-NDefines.NCountry.CAN_CONVERT_TERRITORY_RELIGION = 1 -- Defines if you are allowed to convert religion of territory provinces
-NDefines.NEconomy.BANKRUPTCY_BUILDING_DESTRUCTION_THRESHOLD = 10
-NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = -1.5
-NDefines.NMilitary.MERCENARY_SUPPORT_LIMIT_FRACTION = 0.2
-NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
-NDefines.NReligion.HARMONY_LOSS_PER_DEV_CONVERTED = 0.5
+--- Lag Fix
+NDefines.NEngine.EVENT_PROCESS_OFFSET = 30 ---Vanilla 25
 
---QoL and less roach Changes as of 12/05 above is aswell, just not as recent
 
+--Diplomacy
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10
+NDefines.NDiplomacy.DISHONORABLE_PEACE_MONTHS = 0
+NDefines.NDiplomacy.DESIRED_NUM_OF_ELECTORS = 8
+NDefines.NDiplomacy.MAX_NUM_OF_ELECTORS = 8
+NDefines.NDiplomacy.JOIN_HRE_DEVELOPMENT_CAP_VASSAL = 200
+NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.05			
+NDefines.NDiplomacy.INTEGRATE_UNION_MIN_YEARS = 30 --Mainly for burgundy
 NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
 NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 12
-NDefines.NCountry.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0
+NDefines.NDiplomacy.AE_HRE_INTERNAL = 0.35
+
+--Country
+NDefines.NCountry.PROSPERITY_MONTHLY_DECLINE = -2			
+NDefines.NCountry.PROSPERITY_INCREASE_SIZE = 2
 NDefines.NCountry.MONARCH_MIN_SKILL = 1
-NDefines.NCountry.PROSPERITY_INCREASE_SIZE= 2
-NDefines.NCountry.ESTATE_PROVINCE_HAPPINESS_DECREASE = 3.0
+NDefines.NCountry.DISINHERIT_PRESTIGE_HIT = -25
+NDefines.NCountry.LAND_FORCELIMIT_EXTRA_COST_FACTOR = 2.5
+NDefines.NCountry.SCRIPTED_ADVISOR_DISCOUNT = 0.7  
 NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.4
-NDefines.NEconomy.DEBASE_MAX_CORRUPTION = 30
-NDefines.NEconomy.BANKRUPTCY_PROVINCE_DEVASTATION_GAIN = 20
 NDefines.NCountry.PS_REPLACE_RIVAL = 0
 
+--NationMilitry
 
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
 NDefines.NMilitary.SLACKEN_AP_DROP = 0.05
 NDefines.NMilitary.SLACKEN_MANPOWER_INCREASE = 1.5
-NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.25
-NDefines.NMilitary.NOMAD_PLAINS_SHOCK_BONUS = 0.10			
-NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.0
-NDefines.NMilitary.NAVAL_CASUALTY_MIN_MORALE_DAMAGE = 0.2
+NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.30
 NDefines.NMilitary.CREATE_JANISSARIES_MIL_COST = 8
+NDefines.NMilitary.ARMY_DRILL_YEARLY_DECAY = -1.5
 
 
---NNationDesigner = {
-	--FLAT_TAX_AMOUNT = 4,					-- Flat tax
-	--FLAT_MP_AMOUNT = 3,	
+--Economy
+NDefines.NEconomy.BANKRUPTCY_BUILDING_DESTRUCTION_THRESHOLD = 10
+NDefines.NEconomy.DEBASE_MAX_CORRUPTION = 30
+NDefines.NEconomy.BANKRUPTCY_PROVINCE_DEVASTATION_GAIN = 20
 
--- PO_HUMILIATE_POWER_GAIN = 100, -- potential change in the future
--- MAXIMUM_CONDOTTIERI = 20,
+--Religion (Protestant rework)
+NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 2
+NDefines.NReligion.MAX_RELIGIOUS_CENTER_SPREAD_DISTANCE = 75.0			-- When spreading the religion to other provinces the distance wont exceed this number
+NDefines.NReligion.CONVERSION_ZEAL_DURATION = 7300
+
+NDefines.NReligion.MAX_CHURCH_POWER = 250
+NDefines.NReligion.ASPECT_REMOVE_COST = 0										-- Stabhit for removing an Aspect of Faith.
+NDefines.NReligion.MAX_UNLOCKED_ASPECTS = 4									-- Maximum number of Aspects of Faith the player can have unlocked at once.
+NDefines.NReligion.CHURCH_POWER_RATE_SCALE = 0.05
